286bdb0e853dc87635af10ed637660ee63a055c2
Bartomeu Mas <bartommeumas00@gmail.com>
2023-11-03 17:10:24 +0100

    Initial Commit

---
+++ Smash Notes/Smash_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smash_NotesApp.swift
+//  Smash Notes
+//
+//  Created by Bartomeu Mas Castillo on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Smash_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
